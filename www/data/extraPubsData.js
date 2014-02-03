54dd4fd64c4948fbd867a8b136a01e31bb3f87c0
Dave E <dave@brewmook.com>
2014-02-03 23:56:06 +0000

    Pulled out additional pubs so that overpassData can be used directly

---
+++ www/data/extraPubsData.js
@@ -0,0 +1,6 @@
+var extraPubsData = [
+{"id":910426589,"lat":55.9444999,"lon":-3.1852839,"name":"Counting House"},
+{"id":2076668725,"lat":55.9591358,"lon":-3.2144495,"name":"The Scran & Scallie"},
+{"id":2100296385,"lat":55.9525557,"lon":-3.2041238,"name":"Living Room"},
+{"id":2426508097,"lat":55.9473369,"lon":-3.1859154,"name":"Aspen"},
+];
