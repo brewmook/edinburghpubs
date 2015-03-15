866a9da519620dd0316c35ebd60f4be6f93ddf45
Dave E <dave@brewmook.com>
2015-03-15 00:53:57 +0000

    Attempt to fix browser cacheing.

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,14 @@
 requirejs.config({
-    "baseUrl": "js/lib",
-    "paths": {
-        "app": "../app",
-        "data": "../../data",
-        "leaflet": "http://cdn.leafletjs.com/leaflet-0.7.3/leaflet",
-        "voronoi": "rhill-voronoi-core"
+    baseUrl: "js/lib",
+    urlArgs: "v=e8510bd2fa61f371a93c74cbfe4f941001109f35",
+    paths: {
+        app: "../app",
+        data: "../../data",
+        leaflet: "http://cdn.leafletjs.com/leaflet-0.7.3/leaflet",
+        voronoi: "rhill-voronoi-core"
     },
-    "shim": {
-        "voronoi": { "exports": "Voronoi" }
+    shim: {
+        voronoi: { "exports": "Voronoi" }
     }
 });
 
