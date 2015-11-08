8c38ce14e5d5726d2bdc379805d48d7483ee00eb
Dave E <dave@brewmook.com>
2015-11-08 23:10:20 +0000

    Added requirejs build+optimise script.

---
+++ build.js
@@ -0,0 +1,14 @@
+({
+    appDir: "www",
+    baseUrl: "js",
+    dir: "build",
+    removeCombined: true,
+    paths: {
+        data: "empty:",
+        leaflet: "empty:",
+        voronoi: "empty:"
+    },
+    modules: [
+        {name: "app"}
+    ]
+})
