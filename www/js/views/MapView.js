6d535b739c44cbcd8561db0fa2ce27029746c745
Dave E <dave@brewmook.com>
2016-01-25 23:38:46 +0000

    Switched to Mapbox

--- www/js/views/MapView.js
+++ www/js/views/MapView.js
@@ -7,12 +7,12 @@ function (leaflet) {
     function MapView(mapDivId)
     {
         this.map = leaflet.map(mapDivId);
-
-        // Open Street Map attribution
-        var osmAttr = '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>';
         leaflet.tileLayer(
-            'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png',
-            { attribution: osmAttr }
+            'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png32?access_token={accessToken}',
+            {
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                accessToken: 'pk.eyJ1IjoiYnJld21vb2siLCJhIjoiY2lqdWxoam42MGY4bXRzbTh2OXIzZDM1aCJ9.3Y4fgDB4imI3usjQWobQDA'
+            }
         ).addTo(this.map);
     }
 
