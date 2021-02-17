627d037918d4bda68c7e92da65d2342a3a145770
Dave E <6138098+brewmook@users.noreply.github.com>
2021-02-17 21:45:49 +0000

    Fix map tiles (thanks Colin)

--- www/js/views/MapView.js
+++ www/js/views/MapView.js
@@ -8,8 +8,9 @@ function (leaflet) {
     {
         this.map = leaflet.map(mapDivId);
         leaflet.tileLayer(
-            'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png32?access_token={accessToken}',
+            'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
             {
+                id: 'mapbox/streets-v11',
                 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
                 accessToken: 'pk.eyJ1IjoiYnJld21vb2siLCJhIjoiY2lqdWxoam42MGY4bXRzbTh2OXIzZDM1aCJ9.3Y4fgDB4imI3usjQWobQDA'
             }
